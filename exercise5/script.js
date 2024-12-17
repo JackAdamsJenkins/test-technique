1f03001c075ec0664ea152f013e633faf3631b87
JackAdamsJenkins <clensx@gmail.com>
2024-12-17 08:52:39 +0100

    fix: Improve insertion sort algorithm
    Correct data type declaration for `key` variable.

--- exercise5/script.js
+++ exercise5/script.js
@@ -1,7 +1,7 @@
 function insertionSort() {
     const inputArray = document.getElementById('inputArray').value.split(',').map(Number);
     for (let i = 1; i < inputArray.length; i++) {
-        let key = inputArray[i];
+        const key = inputArray[i];
         let j = i - 1;
         while (j >= 0 && inputArray[j] > key) {
             inputArray[j + 1] = inputArray[j];
