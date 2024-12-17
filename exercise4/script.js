6ddefcb2eac3dc808b20bc3fb3abc2ebff44b2d8
JackAdamsJenkins <clensx@gmail.com>
2024-12-17 08:51:14 +0100

    feat: Implement FizzBuzz application
    Add a webpage displaying the FizzBuzz sequence
    from 1 to 100.  Improve visual presentation
    with basic styling.

---
+++ exercise4/script.js
@@ -0,0 +1,17 @@
+function fizzBuzz() {
+    let output = '';
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            output += 'FizzBuzz<br>';
+        } else if (i % 3 === 0) {
+            output += 'Fizz<br>';
+        } else if (i % 5 === 0) {
+            output += 'Buzz<br>';
+        } else {
+            output += i + '<br>';
+        }
+    }
+    document.getElementById('result').innerHTML = output;
+}
+
+fizzBuzz();
